b5d61a274979e943672ec55cbd4d79967eb2d5ff
hashashin <gentoo.power@gmail.com>
2017-10-20 18:04:56 +0200

    update busybox, openvpn

--- UtilsUpdate/remove.sh
+++ UtilsUpdate/remove.sh
@@ -33,7 +33,7 @@ rm -f /bin/cal
 ln -sf /bin/busybox /bin/ash
 ln -sf /bin/busybox /bin/sh
 # busybox itself
-rm -f /bin/busybox-1.27.1
+rm -f /bin/busybox-1.27.2
 
 # wget
 ln -sf $MODULES/bin/wget /usr/bin/wget
